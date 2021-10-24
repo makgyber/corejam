9c243656fcbde7edb6cffb592fd2182de8c1b611
Jon Mereria <makgyber@gmail.com>
2021-10-24 23:31:01 +0800

    fix table ordering

--- resources/views/dashboard/activities/index.blade.php
+++ resources/views/dashboard/activities/index.blade.php
@@ -23,6 +23,7 @@
                         <table class="table table-responsive-sm table-bordered table-condensed">
                         <thead class="thead-dark">
                           <tr>
+                            <th rowspan="2">#</th>
                             <th rowspan="2">Title</th>
                             <th rowspan="2">Success Indicator</th>
                             <th rowspan="2">Location</th>
@@ -44,16 +45,17 @@
 
                           @forelse($target->activities ?? '' as $activity)
                             <tr>
+                              <td>{{$activity->id}}</td>
                               <td><strong>{{ $activity->title }}</strong></td>
                               <td>{{ $activity->success_indicator }}</td>
                               <td>{{ $activity->location }}</td>
                               <td>{{ date('Y-m-d', strtotime($activity->target_start)) }} to {{ date('Y-m-d', strtotime($activity->target_end)) }}</td>
-                              <td>{{ $activity->remarks }}</td>
                               <td>{{ $activity->plan_b }}</td>
                               <td>{{ $activity->support_request }}</td>
                               <td>{{ $activity->support_from_whom }}</td>
                               <td>{{ $activity->support_how_much }}</td>
                               <td>{{ $activity->support_when_needed }}</td>
+                              <td>{{ $activity->remarks }}</td>
                               <td>
                                 <a href="{{ route('activities.edit', [$target->id,$activity->id]) }}" class="btn btn-block btn-primary btn-sm">Edit</a>
                               </td>
