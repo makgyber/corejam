5bd9f9fa4dc52951ed51dd64801d3f7087677673
Jon Mereria <makgyber@gmail.com>
2021-10-24 23:54:25 +0800

    cleanup

--- app/Http/Requests/CreateActivityRequest.php
+++ app/Http/Requests/CreateActivityRequest.php
@@ -28,15 +28,15 @@ class CreateActivityRequest extends FormRequest
             'target_id' => 'required',
             'title' => 'required',
             'success_indicator' => 'required',
-            'location' => 'required',
+            'location' => '',
             'remarks' => '',
-            'plan_b' => 'required',
+            'plan_b' => '',
             'support_request' => '',
             'support_from_whom' => '',
             'support_how_much' => '',
             'support_when_needed' => '',
-            'target_start' => '',
-            'target_end' => '',
+            'target_start' => 'required',
+            'target_end' => 'required',
         ];
     }
 }
