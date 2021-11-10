e90bf491173fe0bf1d95354a1b2d8cc0e41ba071
Jon Mereria <makgyber@gmail.com>
2021-11-11 07:37:26 +0800

    update mail message

--- app/Notifications/CoordinatorInviteNotification.php
+++ app/Notifications/CoordinatorInviteNotification.php
@@ -47,11 +47,22 @@ class CoordinatorInviteNotification extends Notification
                     ->line('Fill out the form with correct current information and submit it.')
                     ->line('After which, you can login using your registered email and password to www.m610.ph to view your Dashboard.')
                     ->action('Accept invitation', $this->url)
+                    ->line('Then, type in your new Password and then, please follow these steps: ')
+                    ->line('1.You be directed to EDIT PROFILE. Fill out the form with correct current information and submit it. ')
+                    ->line('2. After which, you can login using your registered email and password to www.m610.ph to view your Dashboard.')
+                    ->line('3. On your Dashboard, click Church/Affiliations.')
+                    ->line('4. Click Add affiliation  and type in your church or organization information in the blank fields;')
+                    ->line('5.Then, click Save and Return')
+                    ->line('6.On the Affiliation Table, click the "Members" button;')
+                    ->line('7. You will be redirected to the Member Registry and then, START registering your church members 
+                    who are registered voters who have decided and committed to vote 
+                    for Sen.Manny Pacquiao for President in their hearts.')
+                    ->line('If you have questions, feel free to message us at info.m610@gmail.com. Thank you very much!')
                     ->salutation("Blessings, ")
                     ->from('info.m610@m610.com', 'Admin M6:10');
                     
     }
-
+               
     /**
      * Get the array representation of the notification.
      *
