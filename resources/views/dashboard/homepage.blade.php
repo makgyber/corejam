85de7afaf7b4a3acc289026d9d2bf01351d316c2
Jon Mereria <makgyber@gmail.com>
2021-11-13 15:27:36 +0800

    add data to dashboard

--- resources/views/dashboard/homepage.blade.php
+++ resources/views/dashboard/homepage.blade.php
@@ -83,7 +83,56 @@
                 </div>
                 <!-- /.col-->
               </div>
-
+              <div class="row">
+                <div class="col-sm-6 col-lg-6">
+                  <div class="card mb-4" style="--cui-card-cap-bg: #3b5998">
+                    <div class="card-header position-relative d-flex justify-content-center align-items-center bg-gradient-warning text-white">
+                      <h4>
+                      <i class="cil-wc"></i><span class="ml-3">Gender</span> </h4>
+                    </div>
+                    <div class="card-body row text-center">
+                      <div class="col">
+                        <div class="fs-5 fw-semibold">{{$totals['male'] ?? 0}}</div>
+                        <div class="text-uppercase text-medium-emphasis small">male</div>
+                      </div>
+                      <div class="vr"></div>
+                      <div class="col">
+                        <div class="fs-5 fw-semibold">{{$totals['female'] ?? 0}}</div>
+                        <div class="text-uppercase text-medium-emphasis small">female</div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <!-- /.col-->
+                <div class="col-sm-6 col-lg-6">
+                  <div class="card mb-4" style="--cui-card-cap-bg: #00aced">
+                    <div class="card-header position-relative d-flex justify-content-center align-items-center bg-gradient-success text-white">
+                      <h4>
+                        <i class="cil-wc"></i><span class="ml-3">Age</span> </h4>
+                    </div>
+                    <div class="card-body row text-center">
+                      <div class="col">
+                        <div class="fs-5 fw-semibold">{{$totals['youth']??0}}</div>
+                        <div class="text-uppercase text-medium-emphasis small">youth  (18-29)</div>
+                      </div>
+                      <div class="vr"></div>
+                      <div class="col">
+                        <div class="fs-5 fw-semibold">{{$totals['adults']??0}}</div>
+                        <div class="text-uppercase text-medium-emphasis small">adults (30-59)</div>
+                      </div>
+                      <div class="vr"></div>
+                      <div class="col">
+                        <div class="fs-5 fw-semibold">{{$totals['seniors']??0}}</div>
+                        <div class="text-uppercase text-medium-emphasis small">seniors (60 and up)</div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <!-- /.col-->
+                
+                </div>
+                <!-- /.col-->
+              </div>
               <!-- /.row-->
               <div class="row">
                 <div class="col-md-12">
