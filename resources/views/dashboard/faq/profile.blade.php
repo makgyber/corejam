3146bc9e3307fb0b22c522ef91fd4d3e3098ccd6
Jon Mereria <makgyber@gmail.com>
2021-11-14 15:36:03 +0800

    start page

--- resources/views/dashboard/faq/profile.blade.php
+++ resources/views/dashboard/faq/profile.blade.php
@@ -4,14 +4,124 @@
 
 <div class="animated fadeIn">
     <div class="row card col-12">
-        <div class="card-header"><strong>Updating your personal profile data</strong></div>
+        <div class="card-header"><h4>Updating your personal profile data</h4></div>
         <div class="card-body">
+            <div class="row mb-5">
+                <div class="col-4">
+                    <h5>The Profile Landing Page</h5>  
+                    The Profile landing page provides a quick look your basic information, groups and target outcomes.
+                    The three buttons on upper right corners of the different sections will lead directly to the named modules 
+                    for easy access to the different functionalities.
+                </div>
+                <div class="col-8">
+                    
+                    <img src="{{asset('assets/faq/profile/complete.png')}}" alt="" class="col-12">
+                </div>          
+            </div>
+
+
+<h5>The Edit Profile page consists of 4 basic sections</h5>            
+
+<ul class="list-group">
+    <li class="list-group-item">
+        <h5>Edit Profile</h5>
+        <div class="row">
+        <div class="col-6">
+        <table class="table table-sm  table-condensed table-outline table-hover">
+            <tbody>
+                <tr>
+                    <th>Field</th>
+                    <th>Description</th>
+                    <th>Notes</th>
+                    <th>Example</th>
+                </tr>
+                <tr>
+                    <td>First Name</td>
+                    <td></td>
+                    <td>required</td>
+                    <td>Juan</td>
+                </tr>
+                <tr>
+                    <td>Middle Name</td>
+                    <td></td>
+                    <td>preferred</td>
+                    <td>Cruz</td>
+                </tr>
+                <tr>
+                    <td>Last Name</td>
+                    <td></td>
+                    <td>required</td>
+                    <td>Santos</td>
+                </tr>
+                <tr>
+                    <td>Birthday</td>
+                    <td>used for calculating age demographics</td>
+                    <td>required</td>
+                    <td>01/01/1970</td>
+                </tr>
+                <tr>
+                    <td>Email</td>
+                    <td>used for login and authentication; can be changed but should be unique across the whole system</td>
+                    <td>required</td>
+                    <td>juancruzsantos@mail.xyz</td>
+                </tr>
+                <tr>
+                    <td>Contact Number</td>
+                    <td>for direct communications</td>
+                    <td>preferred</td>
+                    <td>0917-1238467</td>
+                </tr>
+                <tr>
+                    <td>Residential Address</td>
+                    <td>full address for record purposes and communications</td>
+                    <td>preferred</td>
+                    <td>100 JP Rizal St. Brgy A</td>
+                </tr>
+                <tr>
+                    <td>Gender</td>
+                    <td>used for gender demographics calculation</td>
+                    <td>required</td>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    <div class="col-6">
+        <img src="{{asset('assets/faq/profile/editprofile.png')}}" alt="" class="col-8">
+    </div>
+</div>
+    </li>
+    <li class="list-group-item">
+        <h5>Skills and Capabilities</h5>
+        <img src="{{asset('assets/faq/profile/skills.png')}}" alt="" width="400">
+        <table>
+            <tbody>
+                <tr>
+                    <th>Field</th>
+                    <th>Description</th>
+                    <th>Notes</th>
+                    <th>Example</th>
+                </tr>
+            </tbody>
+        </table>
+    </li>
+    <li class="list-group-item">
+        <h5>Registered Voter's Adddress</h5>
+        <img src="{{asset('assets/faq/profile/voters.png')}}" alt="" width="400" >
+    </li>
+    <li class="list-group-item">
+        <h5>Information for Business Owners</h5>
+        <img src="{{asset('assets/faq/profile/business.png')}}" alt="" width="400">
+    </li>
+</ul>
+
+
 
         </div>
     </div>
     <div class="position-absolute-bottom">
         <nav class="navbar navbar-light bg-light">
-            <div class="d-flex justify-content-start"><a class="btn btn-sm btn-dark" href="{{route('faq', 'profile')}}"><i class="cil-hand-point-left"></i>  Updating your personal profile data</a></div>
+            <div class="d-flex justify-content-start"><a class="btn btn-sm btn-dark" href="{{route('faq', 'start')}}"><i class="cil-hand-point-left"></i>  Quick Start</a></div>
             <div class="d-flex justify-content-center"><a class="btn btn-sm btn-dark" href="{{route('faq', 'index')}}"><i class="cil-hamburger-menu"></i> Table of contents</a></div>
             <div class="d-flex justify-content-end"><a class="btn btn-sm btn-dark" href="{{route('faq', 'affiliations')}}"><i class="cil-hand-point-right"></i>  Managing your affiliations</a></div>
       </nav>
--- resources/views/dashboard/faq/start.blade.php
+++ resources/views/dashboard/faq/start.blade.php
@@ -4,47 +4,62 @@
 
 <div class="container-fluid animated fadeIn">
     <div class="animated fadeIn">
-        <div class="row card col-12">
-            <div class="card-header"><strong>Where to start?</strong></div>
-            <div class="card-body">
-<h3>Welcome to your CMS Dashboard</h3>
-<h4>Quick start</h4>
-<p>If this is your first time here, this page will guide you through the initial steps required to complete setting up your profile.</p>
-<ol>
-    <li>
-        Complete the profile information on your Profile page. <a href="{{route('profile.edit')}}" class="btn btn-sm btn-light">Edit profile</a>
+        <div class="row">
+            <div class="card col-5">
+                <div class="card-header"><strong>Where to start?</strong></div>
+                <div class="card-body">
+                    <h3>Welcome to your CMS Dashboard</h3>
+                    <h4>Quick start</h4>
+                    <p>If this is your first time here, this page will guide you through the initial steps required to complete setting up your profile.</p>
 
-    </li>
-    <li>
-        After providing the required data, you need to register your church in the system. 
-        <a href="{{route('affiliations.create')}}" class="btn btn-sm btn-light">Add affiliation</a>
-    </li>
-    <li>Once you have at least one affiliation registered, you can now input members for that organisation.  
-        <a href="{{route('affiliations.create')}}" class="btn btn-sm btn-light">Go to Members Registry</a>
+                    <ol>
+                        <li>
+                            You will first need to 
+                            complete the profile information on your Profile page. It will look like the image on the right.
+                            You can go do that now by clicking on this link:
+                            <a href="{{route('profile.edit')}}" class="btn btn-sm btn-light" target="_blank">Edit profile</a>
 
-    </li>
-    <li>As a coordinator, you will also need to record your Target Outcomes to the system.
-        <a href="{{route('affiliations.create')}}" class="btn btn-sm btn-light">Add Target Outcome</a>
+                        </li>
+                        <li>
+                            After providing the required data, you need to register your church in the system. Notice that the image on the right prompts you
+                            to add your Church or affiliation by clicking on the link. You can also do that by clicking on this link:
+                            <a href="{{route('affiliations.create')}}" class="btn btn-sm btn-light">Add affiliation</a>
+                        </li>
+                        <li>Once you have at least one affiliation registered, you can now input members for that organisation.  
+                            <a href="{{route('members.index')}}" class="btn btn-sm btn-light">Go to Members Registry</a>
 
-    </li>
-    <li>Critical activities to achieve your Target Outcomes will then be defined and added. 
-        This is also where request for support can be specified.
-        <br>
-        <em> Please note that since activities are linked to target outcomes, there is no menu item in the side bar that
-        directly leads to the activity module. This button will be available only after a target outcome has been created.</em>
+                        </li>
+                        <li>As a coordinator, you will also need to record your Target Outcomes to the system.
+                            <a href="{{route('targets.create')}}" class="btn btn-sm btn-light">Add Target Outcome</a>
 
-    </li>
-</ol>
+                        </li>
+                        <li>Critical activities to achieve your Target Outcomes will then be defined and added. 
+                            This is also where request for support can be specified.
+                            <br>
+                            <em> Please note that since activities are linked to target outcomes, there is no menu item in the side bar that
+                            directly leads to the activity module. This button will be available only after a target outcome has been created.</em>
+
+                        </li>
+                    </ol>
+                </div>
+                <div class="card-body border rounded-pill mb-2 bg-gradient-warning">
+                        <span class="badge bg-info rounded text-white p-1">Quick Tip</span>
+                        The module titles at the top of the page have a clickable context-help icon <a class="badge bg-light rounded-pill text-dark">?</a>
+                        that will open up the User Guide page.
+                </div>
+            </div>
+
+            <div class="card col-7">
+                <img src="{{asset('assets/faq/start/profile_incomplete.png')}}" alt="" class="col-12" class="d-flex">
+            </div>
+            <div class=" col-12">
+            <nav class="d-flex navbar navbar-light bg-light">
+                <div class="d-flex justify-content-start"><a class="btn btn-sm btn-dark" href="{{route('faq', 'index')}}"><i class="cil-hamburger-menu"></i>   Table of contents</a></div>
+                <div class="d-flex justify-content-end"><a class="btn btn-sm btn-dark" href="{{route('faq', 'profile')}}"><i class="cil-hand-point-right"></i>  Updating your personal profile data</a></div>
+            </nav>
             </div>
         </div>
-        <div class="position-absolute-bottom">
-            <nav class="navbar navbar-light bg-light">
-                <div class="d-flex justify-content-start"><a class="btn btn-sm btn-dark" href="{{route('faq', 'index')}}"><i class="cil-hamburger-menu"></i>   Table of contents</a></div>
 
-                <div class="d-flex justify-content-end"><a class="btn btn-sm btn-dark" href="{{route('faq', 'profile')}}"><i class="cil-hand-point-right"></i>  Updating your personal profile data</a></div>
-          </nav>
-        </div>
-        
     </div>
 </div>
 
