cd202fa74b26b2d8b43a56da1bf96867b04bab91
Jon Mereria <makgyber@gmail.com>
2021-11-18 19:57:27 +0800

    tool kit

---
+++ resources/views/dashboard/faq/campaign-tool-kit.blade.php
@@ -0,0 +1,156 @@
+@extends('dashboard.base')
+
+@section('content')
+
+<div class="animated fadeIn">
+    <div class="row card col-12">
+        <div class="card-header"><strong>SMP Media Kit Google Drive</strong></div>
+        <div class="card-body">
+<h2>SMP Media Kit Google Drive</h2>
+
+<a target="_blank" href="https://drive.google.com/drive/folders/1bPYQBxpEYrMxBYWYYs3AbWh1HjzwKEmK?usp=sharing"
+class="btn btn-sm btn-info">Click here</a>
+
+
+<h2 class="mt-3">Directory:</h2>
+<ol>
+    <li class="mt-4">
+        <h5>MANNY PACQUIAO MOVIE</h5>
+        <a target="_blank" href="https://drive.google.com/file/d/1yDmNj8idCg_EJ1nOrMVS5rLPuj4CN5-W/view?usp=sharing"
+        class="btn btn-sm btn-info">Click here</a>
+    </li>
+
+    <li class="mt-4">
+        <h5>Pacman Villages</h5>
+        <a target="_blank" href="https://drive.google.com/file/d/1--hokNOGseSscvsNHz6Eon6Z6TyfVKpc/view?usp=sharing"
+        class="btn btn-sm btn-info">Click here</a>
+    </li>
+
+    <li class="mt-4">
+        <h5>MP One on One</h5>
+        <a target="_blank" href="https://drive.google.com/file/d/1IcwA6wBHA0QYvdwsD9KqIHY8H-IsqCoG/view?usp=sharing"
+        class="btn btn-sm btn-info">Click here</a>
+    </li>
+    <li class="mt-4">
+        <h5>Oxford</h5>
+        <a target="_blank" href="https://drive.google.com/file/d/1zrKpJuiHdDTcfMz4H22uH1qn9VIr2OFJ/view?usp=sharing"
+        class="btn btn-sm btn-info">Click here</a>
+    </li>
+    <li class="mt-4">
+        <h5>SMP Para Sayo MTV</h5>
+        <a target="_blank" href="https://drive.google.com/file/d/1QfRfCQCPD_QuV4PZF5wlVdc_rB0BJdkD/view?usp=sharing"
+        class="btn btn-sm btn-info">Click here</a>
+    </li>
+    <li class="mt-4">
+        <h5>Salamat po short MTV</h5>
+        <a target="_blank" href="https://drive.google.com/file/d/16d9RH_gVuWRyIacpC8yLdHkRYLowx-PW/view?usp=sharing"
+        class="btn btn-sm btn-info">Click here</a>
+    </li>
+    <li class="mt-4">
+        <h5>MP on Utang ng Bansa Legal Meeting</h5>
+        <a target="_blank" href="https://drive.google.com/file/d/1a641s7rT63QR-Sq5khHMs8LZ3iRKJng7/view?usp=sharing"
+        class="btn btn-sm btn-info">Click here</a>
+    </li>
+    <li class="mt-4">
+        <h5>SMP why do i box</h5>
+        <a target="_blank" href="https://drive.google.com/file/d/1rDsXCsekPWvFDZ2iihGIFbViyPOmIRsd/view?usp=sharing"
+        class="btn btn-sm btn-info">Click here</a>
+    </li>
+    <li class="mt-4">
+        <h5>Manny Pacquiao tapat na panunungkulan</h5>
+        <a target="_blank" href="https://drive.google.com/file/d/1BvjsjDn-7VIfKagfAT8jqoLBF2zsJyir/view?usp=sharing"
+        class="btn btn-sm btn-info">Click here</a>
+    </li>
+    <li class="mt-4">
+        <h5>Kibawe SMP Homecoming</h5>
+        <a target="_blank" href="https://drive.google.com/file/d/1gdv_r76aqmOQVt1Z8kpNEPyFreOx7oUu/view?usp=sharing"
+        class="btn btn-sm btn-info">Click here</a>
+    </li>
+    <li class="mt-4">
+        <h5>PACMAN VILLAGES PABAHAY</h5>
+        <a target="_blank" href="https://drive.google.com/file/d/1F0utiy1CwDM0tyDa1ROVElv1DuNGGuAA/view?usp=sharing"
+        class="btn btn-sm btn-info">Click here</a>
+    </li>
+
+    <li class="mt-4">
+        <h5>MANNY MTV - LALAYA KA (Freddie Aguilar)</h5>
+        <a target="_blank" href="https://drive.google.com/file/d/1nA_WqXqR6ANQ52oL56NGg1SdoPy65iPL/view?usp=sharing"
+        class="btn btn-sm btn-info">Click here</a>
+    </li>
+
+    <li class="mt-4">
+        <h5>MANNY mtv PARA SAYO plus COVID-19 RESPONSE</h5>
+        <a target="_blank" href="https://drive.google.com/file/d/1zd8XXn2cSDMiltLueY9gaB2TN9SjXmtc/view?usp=sharing"
+        class="btn btn-sm btn-info">Click here</a>
+    </li>
+    <li class="mt-4">
+        <h5>MP Farmers Meet.mp4</h5>
+        <a target="_blank" href="https://drive.google.com/file/d/1NOTg9WlDEINdosFzVCQ3XS5f0qVjRHQr/view?usp=sharing"
+        class="btn btn-sm btn-info">Click here</a>
+    </li>
+    <li class="mt-4">
+        <h5>Manny Pacquiao pabahay at hanapbuhay susi sa kapayapaan</h5>
+        <a target="_blank" href="https://drive.google.com/file/d/1Bt9AHXrqm2cfr4gm4fFN2dtmZh1vy5bI/view?usp=sharing"
+        class="btn btn-sm btn-info">Click here</a>
+    </li>
+    <li class="mt-4">
+        <h5>Manny Pacquiao_on Criminal Justice System</h5>
+        <a target="_blank" href="https://drive.google.com/file/d/18FdAsIvvLsNOzqVUKXlquQtijD0N0OOR/view?usp=sharing"
+        class="btn btn-sm btn-info">Click here</a>
+    </li>
+    <li class="mt-4">
+        <h5>MP on West Phil issue</h5>
+        <a target="_blank" href="https://drive.google.com/file/d/1F3izg8OvNBeIqyhIO8jQ4igfsgg_dnI9/view?usp=sharing"
+        class="btn btn-sm btn-info">Click here</a>
+    </li>
+    <li class="mt-4">
+        <h5>Jolo Sulu</h5>
+        <a target="_blank" href="https://drive.google.com/file/d/1FunF0iFQzvMSNYOkSVlvV_BXD-zpl-iN/view?usp=sharing"
+        class="btn btn-sm btn-info">Click here</a>
+    </li>
+    <li class="mt-4">
+        <h5>Tawi-Tawi</h5>
+        <a target="_blank" href="https://drive.google.com/file/d/1eDzvY-KkJ9MF_QzvO6pPvOBzNIYUMMVT/view?usp=sharing"
+        class="btn btn-sm btn-info">Click here</a>
+    </li>
+    <li class="mt-4">
+        <h5>Mega Prison at Pabahay</h5>
+        <a target="_blank" href="https://drive.google.com/file/d/1U4yMBrHOuM-ZWsTD92h2xrh3LI5wInHf/view?usp=sharingg"
+        class="btn btn-sm btn-info">Click here</a>
+    </li>
+    <li class="mt-4">
+        <h5>Camahalan Family (PACMAN VILLAGE)</h5>
+        <a target="_blank" href="https://drive.google.com/file/d/1jKpr3cAVyAM4F_BJEKV1gRGDbR4WAp39/view?usp=sharing"
+        class="btn btn-sm btn-info">Click here</a>
+    </li>
+    <li class="mt-4">
+        <h5>Pinoy Ako by Andrew E.</h5>
+        <a target="_blank" href="https://drive.google.com/file/d/1HDTAFOop1G5cvJ7YBZ8_aClwWZMXNcNi/view?usp=sharing"
+        class="btn btn-sm btn-info">Click here</a>
+    </li>
+    <li class="mt-4">
+        <h5>Paranaque Relief Operation</h5>
+        <a target="_blank" href="https://drive.google.com/file/d/1nU1QnNQD2CUr-HVcasEAabdSuQ0ylNGb/view?usp=sharing"
+        class="btn btn-sm btn-info">Click here</a>
+    </li>
+    <li class="mt-4">
+        <h5>SMP PRESSCON 070321</h5>
+        <a target="_blank" href="https://drive.google.com/file/d/1FF-CYJyMi_RjPYQyfPt5DDw-0GpEahAx/view?usp=sharing"
+        class="btn btn-sm btn-info">Click here</a>
+    </li>	
+    <li class="mt-4">
+        <h5>MP LOGO </h5>
+        <a target="_blank" href="https://drive.google.com/file/d/15fqgR-B9eohyzsVazj6Y1Hw23L9HrWOq/view?usp=sharing"
+        class="btn btn-sm btn-info">Click here</a>
+    </li>	
+</ol>
+
+</div>
+</div>
+
+@endsection
+
+
+@section('javascript')
+
+@endsection
--- resources/views/dashboard/shared/footer.blade.php
+++ resources/views/dashboard/shared/footer.blade.php
@@ -1,4 +1,4 @@
 <footer class="c-footer">
-  <div><a href="https://m610.ph/">M6:10 Campaign Management System</a> &copy; 2021.</div>
+  <div><a href="https://m610.ph/">The Christian Vote for Senator Manny Pacquiao for President</a></div>
   <div class="ml-auto">Powered by&nbsp;<a href="https://www.quantumtech.com.ph/">Quantum Tech</a></div>
 </footer>
