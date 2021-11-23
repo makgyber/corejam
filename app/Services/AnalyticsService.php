cc4330807d863947a5d592803bfc8dcad5172dfc
Jon Mereria <makgyber@gmail.com>
2021-11-23 20:06:11 +0800

    limit viewers on dashboard

--- app/Services/AnalyticsService.php
+++ app/Services/AnalyticsService.php
@@ -224,8 +224,13 @@ class AnalyticsService
         $coordinators = DB::table('activities')
                         ->select(DB::raw('activities.title, users.name, users.first_name,users.last_name, users.image, users.created_at,regions.name as region_name, activities.support_how_much, activities.disbursed, max(activities.updated_at)'))
                         ->leftJoin('users', 'activities.owner', '=', 'users.id')
-                        ->leftJoin('regions', 'regions.code', '=', 'users.region_code')
-                        ->groupBy(['activities.title','users.name', 'users.first_name','users.last_name','users.image','users.created_at','regions.name','activities.support_how_much', 'activities.disbursed',])
+                        ->leftJoin('regions', 'regions.code', '=', 'users.region_code');
+        
+        if(auth()->user()->hasRole('coordinator')) {
+            $coordinators->where('activities.owner', auth()->user()->id);
+        }
+
+        $coordinators->groupBy(['activities.title','users.name', 'users.first_name','users.last_name','users.image','users.created_at','regions.name','activities.support_how_much', 'activities.disbursed',])
                         ->orderBy('activities.updated_at')
                         ->paginate(20);
         return $coordinators;
