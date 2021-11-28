e79bc0dbd878d97a3ff2bd4e1cf7b100b5fd05ca
Jon Mereria <makgyber@gmail.com>
2021-11-28 19:33:09 +0800

    add internationalization files

---
+++ database/migrations/2021_11_28_112723_alter_user_add_country.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterUserAddCountry extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('country')->nullable();
+            $table->string('state')->nullable();
+            $table->string('world_city')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
