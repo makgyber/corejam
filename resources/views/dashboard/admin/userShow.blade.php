253402c83627065d8b856892881d49d6865bb53f
Jon Mereria <makgyber@gmail.com>
2021-12-20 13:40:14 +0800

    update

--- resources/views/dashboard/admin/userShow.blade.php
+++ resources/views/dashboard/admin/userShow.blade.php
@@ -93,7 +93,7 @@
                         <tr>
                             <th>Barangay Polling Center</th>
                             <td class="">
-                            {{ $user->barangay?: ($user->barangayVotingCenter->name??'')  }}
+                            {{ $user->barangayVotingCenter->name?: ($user->barangay??'')  }}
                             </td>
                         </tr>
                         @else
@@ -126,7 +126,7 @@
                             <th>Address</th>
                             <td>
                                 {{ $user->street??'' }}
-                                {{ $user->barangay?: ($user->barangayVotingCenter->name??'') }}
+                                {{ $user->barangayVotingCenter->name?: ($user->barangay??'') }}
                                 {{ $user->city->name??'' }}
                                 {{ $user->province->name??'' }}
                                 {{ $user->region->name??'' }}
