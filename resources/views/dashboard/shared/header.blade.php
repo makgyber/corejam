5384cb8752eb4bc17413f99b6fe5858dcde4a76d
Jon Mereria <makgyber@gmail.com>
2022-01-29 08:52:43 +0800

    remove logo in phone mode

--- resources/views/dashboard/shared/header.blade.php
+++ resources/views/dashboard/shared/header.blade.php
@@ -2,7 +2,7 @@
       
     <div class="c-wrapper">
       <header class="c-header c-header-light c-header-fixed c-header-with-subheader">
-        <button class="c-header-toggler c-class-toggler d-lg-none mr-auto" type="button" data-target="#sidebar" data-class="c-sidebar-show"><span class="c-header-toggler-icon"></span></button><a class="c-header-brand d-sm-none" href="#"><img class="c-header-brand" src="{{ url('/assets/brand/m610.svg')  }}" width="60" height="36" alt="CoreUI Logo"></a>
+        <button class="c-header-toggler c-class-toggler d-lg-none mr-auto" type="button" data-target="#sidebar" data-class="c-sidebar-show"><span class="c-header-toggler-icon"></span></button>
         <button class="c-header-toggler c-class-toggler ml-3 d-md-down-none" type="button" data-target="#sidebar" data-class="c-sidebar-lg-show" responsive="true"><span class="c-header-toggler-icon"></span></button>
         
         <a href="{{route('index')}}" class="c-header-brand btn btn-light btn-check">Blog Home</a>
