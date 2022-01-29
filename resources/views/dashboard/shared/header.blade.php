42d8f0538fa05b1f42b076874ea3f09fe3bfd087
Jon Mereria <makgyber@gmail.com>
2022-01-29 09:05:24 +0800

    revert

--- resources/views/dashboard/shared/header.blade.php
+++ resources/views/dashboard/shared/header.blade.php
@@ -2,7 +2,7 @@
       
     <div class="c-wrapper">
       <header class="c-header c-header-light c-header-fixed c-header-with-subheader">
-        <button class="c-header-toggler c-class-toggler d-lg-none " type="button" data-target="#sidebar" data-class="c-sidebar-show"><span class="c-header-toggler-icon"></span></button>
+        <button class="c-header-toggler c-class-toggler d-lg-none mr-auto" type="button" data-target="#sidebar" data-class="c-sidebar-show"><span class="c-header-toggler-icon"></span></button>
         <button class="c-header-toggler c-class-toggler ml-3 d-md-down-none" type="button" data-target="#sidebar" data-class="c-sidebar-lg-show" responsive="true"><span class="c-header-toggler-icon"></span></button>
         
         <a href="{{route('index')}}" class="c-header-brand btn btn-light btn-check">Blog Home</a>
